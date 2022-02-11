99dfa555a003255aa67c90de980f00be5b25f569
Travis CI User <travis@example.org>
2022-02-11 19:03:41 +0000

    Drops insights-proxy frm notifications (#226)
    * Drops insights-proxy frm notifications
     - Upgrades frontend-components config
     - Adds env for easy configuration of the environment/api
     - Updated README
    
    * Updates lock file
    
    * Upgrades karma

---
+++ js/564.1644606074702.b43fd374229de0d4f72b.js
@@ -0,0 +1,1 @@
+(self.webpackChunknotifications_frontend=self.webpackChunknotifications_frontend||[]).push([[564,69],{75347:()=>{},36648:()=>{},31777:()=>{},34017:()=>{},59905:()=>{},64604:()=>{}}]);
