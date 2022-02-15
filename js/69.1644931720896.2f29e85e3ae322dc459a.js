2a1e7f52c96bdfb0d190c71cf3ca661d3372f874
Travis CI User <travis@example.org>
2022-02-15 13:31:07 +0000

    Merge pull request #233 from RedHatInsights/master
    Prod update

---
+++ js/69.1644931720896.2f29e85e3ae322dc459a.js
@@ -0,0 +1,1 @@
+(self.webpackChunknotifications_frontend=self.webpackChunknotifications_frontend||[]).push([[69,564],{75347:()=>{},36648:()=>{},31777:()=>{},34017:()=>{},59905:()=>{},64604:()=>{}}]);
