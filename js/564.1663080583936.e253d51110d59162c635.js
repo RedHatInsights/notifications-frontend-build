660ae3fc3ed6b1f697cd4fcfba9db84bf467e49d
Travis CI User <travis@example.org>
2022-09-15 18:34:13 +0000

    NOTIF-787 Update dependencies (#336)

--- js/564.1663080583936.e253d51110d59162c635.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunknotifications_frontend=self.webpackChunknotifications_frontend||[]).push([[564,69],{75347:()=>{},36648:()=>{},31777:()=>{},34017:()=>{},59905:()=>{},64604:()=>{}}]);
